5b2e9fe86e440ac4008105272ae0c61092405676
22127005 <lethienan1106@gmail.com>
2024-11-12 11:35:04 +0700

    Init commit

---
+++ app/src/main/java/admin/App.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package admin;
+
+public class App {
+    public String getGreeting() {
+        return "Hello World! from admin";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
